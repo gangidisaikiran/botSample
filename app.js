8019fa0306a7f965d0306cebb1457d4aa83863b6
Sai Kiran <sagang@microsoft.com>
2016-12-21 20:12:52 +0530

    add option for Profile and requirement

--- app.js
+++ app.js
@@ -23,6 +23,51 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+	function (session, args, next) {
+    	if (!session.userData.name) {
+    		session.beginDialog('/profile');
+    	}
+    	else {
+    		next();
+    	}
+    },
+    function (session, results, next) {
+    	if(!session.userData.requirement) {
+    		session.send('Hi ' + session.userData.name );
+    		session.beginDialog('/requirement')
+    	}
+    	else {
+    		next();
+    	}
+    },
+    function (session, results) {
+    	if(session.userData.name && session.userData.requirement)
+    	{
+	    	session.userData.requirement = null;
+	    	session.send('Unfortuantely I dont support that now! Can I help you with something else?');
+	    	session.beginDialog('/requirement');
+    	}
+    }
+]);
+
+bot.dialog('/profile', [
+	function (session) {
+		builder.Prompts.text(session, 'What is you name?');
+	},
+	function (session, results) {
+		session.userData.name = results.response;
+		session.endDialog();
+	}
+]);
+
+bot.dialog('/requirement', [
+	function (session, results) {
+		builder.Prompts.text(session, 'What can I help you with today');
+    },
+    function (session, results) {
+    	session.userData.requirement = results.response;
+    	session.endDialog();
+    }
+
+]);
